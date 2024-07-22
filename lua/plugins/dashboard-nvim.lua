76c30c8fc389cbb280fba91d0e3cc3c9d30849a8
wp.l <wenpeng.lei@ncsi.com.cn>
2024-07-22 10:12:25 +0800

    chore: show CWD in dashboard

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -1,3 +1,6 @@
+local fs = require("vim.fs")
+local os = require("os")
+
 return {
   {
     "nvimdev/dashboard-nvim",
@@ -18,7 +21,7 @@ return {
 
       opts.header = logo
       opts.items = {}
-      opts.footer = ""
+      opts.footer = os.getenv("PWD")
     end,
     config = false,
   },
