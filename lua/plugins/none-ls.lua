2b78e9f6c8eaa763f1335d075c70889d200c73cf
wp.l <wenpeng.lei@ncsi.com.cn>
2024-07-29 17:24:06 +0800

    chore(config):Update files

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,13 +7,12 @@ return {
     event = "LazyFile",
     enabled = false,
     opts = function(_, opts)
-      local code_spell = require("cspell")
-      local nls = require("null-ls")
+      local cspell = require("cspell")
       opts.root_dir = opts.root_dir
         or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
       -- All we want is just cspell code actions
       opts.sources = {
-        code_spell.code_actions,
+        cspell.code_actions,
       }
     end,
   },
