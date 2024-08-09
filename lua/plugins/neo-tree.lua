4e5119aa69305dedb9cf7ed05ecdfcc27c97146f
wp.l <wenpeng.lei@ncsi.com.cn>
2024-08-09 16:21:21 +0800

    chore: adjust node-tree window height

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,18 +1,22 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        cmd = "Neotree",
-        opts = {
-            window = {
-                position = "float"
-            },
-            filesystem = {
-                filtered_items = {
-                    hide_dotfiles = false,
-                    hide_gitignored = false
-                }
-            }
-        }
-    }
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = "Neotree",
+    opts = {
+      window = {
+        position = "float",
+        popup = {
+          size = { height = "45", width = "45" },
+          position = "8",
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
 }
