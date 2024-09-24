3c17119fe32c28e2a8eb417a5da28c050f0a1c3f
Lei Wen Peng <wenpeng.lei@ncsi.com.cn>
2024-09-24 10:28:27 +0800

    Initial commit

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd({
-  "BufRead",
-}, {
+vim.api.nvim_create_autocmd("BufRead", {
   pattern = { "*" },
   callback = function()
     vim.cmd("TSBufEnable highlight")
