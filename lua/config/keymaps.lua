b06a2069dcf43d3c079e4b371cafdf22b61f216f
wp.l <freemode1614@outlook.com>
2024-11-24 17:08:03 +0800

    Update Config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local Util = require("lazyvim.util")
+local Snacks = require("snacks")
 
 vim.keymap.set("n", "<C-/>", function()
-  Util.terminal(nil, { border = "rounded" })
+  Snacks.terminal(nil, { border = "rounded" })
 end, { desc = "Term with border" })
 
 vim.keymap.set("n", "<C-S>", "<CMD>wa<CR>", { desc = "Save all files" })
