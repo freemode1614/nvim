6dd02beb745286d21dbea7d5eb7415fe28e133ac
wp.l <freemode1614@outlook.com>
2025-02-01 11:29:54 +0800

    remove aider plugin

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -1,28 +1,43 @@
 return {
-  "vuki656/package-info.nvim",
-  requires = {
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("package-info").setup({
-      package_manager = "pnpm",
-    })
-  end,
-  keys = {
-    {
-      "<leader>ns",
-      require("package-info").show(),
-      desc = "Show dependency versions",
+  {
+    "vuki656/package-info.nvim",
+    requires = {
+      "MunifTanjim/nui.nvim",
     },
-    {
-      "<leader>nc",
-      require("package-info").hide(),
-      desc = "Hide dependency versions",
+    keys = {
+      {
+        "<leader>ns",
+        function()
+          require("package-info").show()
+        end,
+        desc = "Show dependency versions",
+      },
+      {
+        "<leader>nc",
+        function()
+          require("package-info").hide()
+        end,
+        desc = "Hide dependency versions",
+      },
+      {
+        "<leader>nt",
+        function()
+          require("package-info").toggle()
+        end,
+        desc = "Toggle dependency versions",
+      },
     },
-    {
-      "<leader>nt",
-      require("package-info").toggle(),
-      desc = "Toggle dependency versions",
-    },
+    config = function()
+      require("package-info").setup({
+        colors = {
+          up_to_date = "#3C4048", -- Text color for up to date dependency virtual text
+          outdated = "#d19a66", -- Text color for outdated dependency virtual text
+          invalid = "#ee4b2b", -- Text color for invalid dependency virtual text
+        },
+        package_manager = "pnpm",
+        autostart = true,
+        hide_unstable_versions = true,
+      })
+    end,
   },
 }
