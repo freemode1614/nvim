12e0015cb2e29e252a411097d36a6490450858fa
freemode <freemode1614@outlook.com>
2025-02-12 15:17:13 +0800

    chore:enhance MD file DX

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,21 +1,30 @@
 return {
-  {
-    "tenxsoydev/vim-markdown-checkswitch",
-    config = function()
-      vim.g.md_checkswitch_style = "cycle"
-    end,
-  },
+  -- {
+  --   "tenxsoydev/vim-markdown-checkswitch",
+  --   config = function()
+  --     vim.g.md_checkswitch_style = "cycle"
+  --   end,
+  -- },
   {
     "bullets-vim/bullets.vim",
     config = function()
       vim.g.bullets_enabled_file_types = { "markdown", "text", "gitcommit", "scratch" }
     end,
   },
+  -- {
+  --   "yelog/marklive.nvim",
+  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  --   lazy = true,
+  --   ft = "markdown",
+  --   opt = {},
+  -- },
   {
-    "yelog/marklive.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    lazy = true,
-    ft = "markdown",
-    -- dev = true,
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
   },
 }
