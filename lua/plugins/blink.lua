283d9ef7e21cb349e379ec3f288ca7f08d9a1406
freemode <freemode1614@outlook.com>
2025-02-18 14:57:16 +0800

    feat(blink): set both preselect and auto_insert to falase

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,13 +1,17 @@
 return {
   {
     "saghen/blink.cmp",
-    ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
-    config = function(_, opts)
-      opts.completion.list.selection = {
-        preselect = false,
-        auto_insert = false,
-      }
-      require("blink.cmp").setup(opts)
-    end,
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = false,
+          },
+        },
+      },
+    },
   },
 }
