7d1ab1e8dab24f1ddb5faff0f89d08021023a7fe
freemode1614 <150514318+freemode1614@users.noreply.github.com>
2025-05-04 22:34:53 +0800

    Error while loading venv-selector

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,22 +1,13 @@
 return {
   "linux-cultist/venv-selector.nvim",
+  enabled = false,
+  branch = "regexp",
   opts = {
-    -- Your options go here
-    -- name = "venv",
-    -- auto_refresh = false
+    name = { "venv", ".venv" },
+    auto_refresh = true,
   },
   event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap",
-    "mfussenegger/nvim-dap-python", --optional
-    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-  },
-  lazy = false,
-  branch = "regexp", -- This is the regexp branch, use this for the new version
-  config = function()
-    require("venv-selector").setup()
-  end,
+  lazy = true,
   keys = {
     { ",v", "<cmd>VenvSelect<cr>" },
   },
