40da65a91eea46321d85bb21205302cebc516dd3
freemode1614 <150514318+freemode1614@users.noreply.github.com>
2025-05-17 23:16:27 +0800

    Disable black for python

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,21 +7,25 @@
 local black_lsp = "black"
 
 return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
+  {
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
+    opts = function(_, opts)
+      local ensure_installed = {}
+      for i = 1, #opts.ensure_installed do
+        if opts.ensure_installed[i] ~= black_lsp then
+          table.insert(ensure_installed, opts.ensure_installed[i])
+        end
+      end
+
+      opts.ensure_installed = ensure_installed
+    end,
+  },
   {
     "mason-org/mason-lspconfig.nvim",
     version = "^1.0.0",
     opts = function(_, opts)
-      local new_ensure_installed = {}
-      local ensure_installed = opts.ensure_installed or {}
-
-      for i = #ensure_installed, 1, -1 do
-        if ensure_installed[i] ~= black_lsp then
-          new_ensure_installed[i] = ensure_installed[i]
-        end
-      end
-
-      opts.ensure_installed = new_ensure_installed
+      print(opts)
     end,
   },
 }
