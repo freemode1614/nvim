2bb496b269b5677ebeb16cfbc857e6c73e936191
freemode1614 <150514318+freemode1614@users.noreply.github.com>
2025-05-18 23:25:18 +0800

    disabled config, just simpely remove extras.formatting.black

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,28 +4,28 @@
 --
 --]]
 
-local black_lsp = "black"
+-- local black_lsp = "black"
 
 return {
   {
     "mason-org/mason.nvim",
     version = "^1.0.0",
-    opts = function(_, opts)
-      local ensure_installed = {}
-      for i = 1, #opts.ensure_installed do
-        if opts.ensure_installed[i] ~= black_lsp then
-          table.insert(ensure_installed, opts.ensure_installed[i])
-        end
-      end
-
-      opts.ensure_installed = ensure_installed
-    end,
+    -- opts = function(_, opts)
+    --   local ensure_installed = {}
+    --   for i = 1, #opts.ensure_installed do
+    --     if opts.ensure_installed[i] ~= black_lsp then
+    --       table.insert(ensure_installed, opts.ensure_installed[i])
+    --     end
+    --   end
+    --
+    --   opts.ensure_installed = ensure_installed
+    -- end,
   },
   {
     "mason-org/mason-lspconfig.nvim",
     version = "^1.0.0",
-    opts = function(_, opts)
-      print(opts)
-    end,
+    -- opts = function(_, opts)
+    --   print(opts)
+    -- end,
   },
 }
