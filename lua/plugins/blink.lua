bbe66a01934f3e61365134e4b3bdfb2cfd4d6e93
wenpeng <wenpeng.lei@ncsi.com.cn>
2026-02-23 13:40:37 +0800

    add avante.nvim

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,7 +3,6 @@ return {
   -- enable = false,
   dependencies = {
     "Kaiser-Yang/blink-cmp-avante",
-    -- ... Other dependencies
   },
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -16,21 +15,5 @@ return {
         },
       },
     },
-    -- sources = {
-    --   default = {
-    --     "avante",
-    --     "lsp",
-    --     "path",
-    --     "luasnip",
-    --     "buffer",
-    --   },
-    --   providers = {
-    --     avante = {
-    --       module = "blink-cmp-avante",
-    --       name = "Avante",
-    --       opts = {},
-    --     },
-    --   },
-    -- },
   },
 }
