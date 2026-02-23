25ca421ab2266c515d164df730a2bc243f699d28
wenpeng <wenpeng.lei@ncsi.com.cn>
2026-02-23 10:44:09 +0800

    chore: update "package-info" plugin config

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -56,10 +56,10 @@ return {
     },
     config = function()
       require("package-info").setup({
-        colors = {
-          up_to_date = "#3C4048", -- Text color for up to date dependency virtual text
-          outdated = "#d19a66", -- Text color for outdated dependency virtual text
-          invalid = "#ee4b2b", -- Text color for invalid dependency virtual text
+        highlights = {
+          up_to_date = { fg = "#3C4048" }, -- Text color for up to date dependency virtual text
+          outdated = { fg = "#d19a66" }, -- Text color for outdated dependency virtual text
+          invalid = { fg = "#ee4b2b" }, -- Text color for invalid dependency virtual text
         },
         icons = {
           enable = true, -- Whether to display icons
