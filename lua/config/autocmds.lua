d7a3bd20f4e20c27f9163a231b73fc1119117d35
freemode1614 <150514318+freemode1614@users.noreply.github.com>
2026-03-29 10:49:16 +0800

    refactor: 移除 cursorline 自定义设置，完全使用主题自带颜色
    - 移除 autocmds.lua 中 cursorline/cursor 高亮设置
    - 完全依赖 nightfox/dawnfox 主题自带颜色
    - 简化配置，由 auto-dark-mode 统一管理主题切换

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -26,36 +26,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- 光标行高亮 - 与 wezterm 主题保持一致
-local function set_cursorline_colors()
-  vim.cmd("hi Normal ctermbg=none")
-  
-  -- nightfox (dark) / dawnfox (light)
-  if vim.o.background == "light" then
-    -- dawnfox 主题
-    vim.cmd("hi CursorLine guibg=#e8e0d8")
-    vim.cmd("hi CursorLineNr guifg=#625c87 guibg=#e8e0d8")
-  else
-    -- nightfox 主题
-    vim.cmd("hi CursorLine guibg=#2b3b51")
-    vim.cmd("hi CursorLineNr guifg=#71839b guibg=#2b3b51")
-  end
-  
-  -- 光标颜色
-  vim.cmd("hi Cursor guibg=#719cd6 guifg=#192330")
-end
 
--- VimEnter 时设置
-vim.api.nvim_create_autocmd("VimEnter", {
-  pattern = { "*" },
-  callback = set_cursorline_colors,
-})
-
--- ColorScheme 切换时也设置（auto-dark-mode 切换主题时触发）
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = { "*" },
-  callback = set_cursorline_colors,
-})
 
 vim.api.nvim_create_autocmd("VimEnter", {
   pattern = { "*" },
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,28 +1,28 @@
 return {
-    "saghen/blink.cmp",
-    -- enable = false,
-    dependencies = {
-        "Kaiser-Yang/blink-cmp-avante",
-    },
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        sources = {
-            -- default = { "lsp", "path", "luasnip", "buffer", --[["avante"]] },
-            providers = {
-                avante = {
-                    module = "blink-cmp-avante",
-                    name = "Avante",
-                },
-            },
+  "saghen/blink.cmp",
+  -- enable = false,
+  dependencies = {
+    "Kaiser-Yang/blink-cmp-avante",
+  },
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    sources = {
+      -- default = { "lsp", "path", "luasnip", "buffer", --[["avante"]] },
+      providers = {
+        avante = {
+          module = "blink-cmp-avante",
+          name = "Avante",
         },
-        completion = {
-            list = {
-                selection = {
-                    preselect = true,
-                    auto_insert = false,
-                },
-            },
-        },
+      },
     },
+    completion = {
+      list = {
+        selection = {
+          preselect = true,
+          auto_insert = false,
+        },
+      },
+    },
+  },
 }
