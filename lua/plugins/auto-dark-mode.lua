212a7dc0a64f7985f82261827232d98f33954622
tzhu <tzhu@outlook.com>
2026-04-24 23:37:39 +0800

    Update plugins configuration

--- lua/plugins/auto-dark-mode.lua
+++ lua/plugins/auto-dark-mode.lua
@@ -6,7 +6,7 @@ return {
     update_interval = 1000,
     set_dark_mode = function()
       vim.o.background = "dark"
-      vim.cmd("colorscheme tokyonight-moon")
+      vim.cmd("colorscheme kanagawa")
     end,
     set_light_mode = function()
       vim.o.background = "light"
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,37 +1,38 @@
-return {
-  {
+return {{
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     cmd = "Neotree",
+    dependencies = {"nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons"},
+    lazy = false,
     opts = {
-      -- recommanded config for better UI
-      hide_root_node = true,
-      retain_hidden_root_indent = true,
-      filesystem = {
-        filtered_items = {
-          show_hidden_count = true,
-          never_show = {
-            ".DS_Store",
-          },
-          hide_dotfiles = false,
+        -- recommanded config for better UI
+        hide_root_node = true,
+        retain_hidden_root_indent = true,
+        filesystem = {
+            filtered_items = {
+                show_hidden_count = true,
+                never_show = {".DS_Store"},
+                hide_dotfiles = false
+            },
+            window = {
+                mappings = {
+                    -- ["<"] = "open_in_system_explorer",
+                }
+            }
+        },
+        follow_current_file = {
+            enabled = true, -- This will find and focus the file in the active buffer every time
+            leave_dirs_open = false -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
         },
         window = {
-          mappings = {
-            -- ["<"] = "open_in_system_explorer",
-          },
-        },
-      },
-      follow_current_file = {
-        enabled = true, -- This will find and focus the file in the active buffer every time
-        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-      },
-      window = {
-        position = "float",
-        popup = {
-          size = { height = "80%", width = "60" },
-          position = "5",
-        },
-      },
-    },
-  },
-}
+            position = "float",
+            popup = {
+                size = {
+                    height = "80%",
+                    width = "60"
+                },
+                position = "5"
+            }
+        }
+    }
+}}
